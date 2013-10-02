eb945848a04bee326c430f366f99c79adb4c3c85
NickFogell <n.fogell09@imperial.ac.uk>
2013-10-02 16:37:02 +0100

    add conference results directory to backup

--- backup_script.sh
+++ backup_script.sh
@@ -7,3 +7,5 @@ sudo rsync -avz /media/Data/Models/star/BT533/FSI/v2-half-cell_vertical_movement
 sudo rsync -avz /media/Data/Models/star/BT533/FSI/v2-half-cell_vertical_movement+external_force/500MPa_isotropic_q_external-force/fluid10/ /media/SimpleDrive/Uni/Models/FSI/3D-CFD/500MPa_iso_q_ext/fluid10
 
 sudo rsync -avz /media/Data/Models/star/BT533/FSI/v2-half-cell_vertical_movement+external_force/500MPa_isotropic_q_external-force/structure10/ /media/SimpleDrive/Uni/Models/FSI/3D-CFD/500MPa_iso_q_ext/structure10
+
+sudo rsync -avz /media/Data/Models/star/BT533/FSI/conference-results/iteration5/fluid5a/altered-y+/00-iterations/ /media/SimpleDrive/Uni/Models/FSI/3D-CFD/conference-results/fluid/
